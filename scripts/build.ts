78a024eda16ebb050d559935bcdcd9fe3d515da5
codethread <adamhalldesigns@gmail.com>
2024-07-26 06:18:18 +0100

    more stuff

---
+++ scripts/build.ts
@@ -0,0 +1,17 @@
+const result = await Bun.build({
+  entrypoints: ["./src/index.ts"],
+  minify: true,
+});
+
+if (!result.success) {
+  console.error("Build failed");
+  for (const message of result.logs) {
+    // Bun will pretty print the message object
+    console.error(message);
+  }
+}
+
+for (const output of result.outputs) {
+  const txt = await output.text();
+  await Bun.write("./build/git-buddy", ["#!/usr/bin/env bun\n\n", txt]);
+}
---
+++ scripts/release.ts
@@ -0,0 +1,23 @@
+import { $ } from "bun";
+import pj from "../package.json";
+
+const [bump] = process.argv.slice(2);
+
+if (!["minor", "major"].includes(bump))
+  throw new Error("expected argument minor or major");
+
+const { name, version, ...pjFields } = pj;
+
+let [major, minor] = version.split(".").map((v) => parseInt(v));
+
+if (bump === "major") major++;
+if (bump === "minor") minor++;
+
+const newVersion = [major, minor, 0].join(".");
+
+await Bun.write(
+  "../package.json",
+  JSON.stringify({ name, version: newVersion, ...pjFields }),
+);
+
+await $`prettier --write package.json`;
